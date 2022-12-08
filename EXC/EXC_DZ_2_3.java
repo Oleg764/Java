c5f538d4e18022b6459b3f89de9a6ae67cb2de24
Oleg764 <oleg_764@mail.ru>
2022-12-08 10:07:53 +0500

    EXCd.z.1-2-3

---
+++ EXC/EXC_DZ_2_3.java
@@ -0,0 +1,24 @@
+package EXC;
+import java.io.FileNotFoundException;
+public class EXC_DZ_2_3 {
+    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+        System.out.println(a + b);
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[3] = 9;
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+}
